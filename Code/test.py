b210415e9473733b5d8e3c5e09979bcb087666b3
No0ne155 <magnus@sunrise.ch>
2023-07-06 13:43:18 +0200

    changed test

--- Code/test.py
+++ Code/test.py
@@ -1,32 +1,10 @@
 import numpy as np
 from math import*
 
-class Cube:
-    def __init__(self,vec) -> None:
-        self.vec = vec
+xp = np.array([0,1,0])
 
-    def rotate(self, angle, axis):
-        rad = radians(angle)
-        c = cos(rad)
-        s = sin(rad)
-        
-        if axis == 'x':
-            y, z = self.y, self.z
-            self.y = y * c - z * s
-            self.z = y * s + z * c
-        elif axis == 'y':
-            x, z = self.x, self.z
-            self.x = x * c + z * s
-            self.z = -x * s + z * c
-        elif axis == 'z':
-            x, y = self.x, self.y
-            self.x = x * c - y * s
-            self.y = x * s + y * c
-
-cube1 = Cube(np.array([-1,-1, 1]))
-
-#cube1.rotate(3, 'x')
-x = cube1.vec[0]
-print(x)
-x = cube1.vec[0]+1
-print(cube1.vec[0])
+v1 = np.copy(xp)
+v2 = np.copy(xp)
+xp[0]=2
+v2[0]=7
+print(v1[0])
