b3ed42344e1f9c7183657dc55d6ae4ea36752d31
No0ne155 <magnus@sunrise.ch>
2023-08-28 08:25:36 +0200

    update data

--- Code/test.py
+++ Code/test.py
@@ -1,8 +1,23 @@
-import numpy as np
-# Open the file in append mode ('a')
-file_path = 'cubedata.txt'
-count = 12861872368
-ts = 9988
-data_to_append = (count, ts)
-with open(file_path, 'a') as file:
-    file.write(f'{ts} sec, {count} turns'+'\n')  # Write the data to the file and add a newline character
+import random
+
+def guess_the_number():
+    secret_number = random.randint(1, 100)
+    attempts = 0
+
+    print("Willkommen bei 'Errate die Zahl'!")
+    print("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht. Kannst du sie erraten?")
+
+    while True:
+        guess = int(input("Dein Tipp: "))
+        attempts += 1
+
+        if guess < secret_number:
+            print("Zu niedrig! Versuche es erneut.")
+        elif guess > secret_number:
+            print("Zu hoch! Versuche es erneut.")
+        else:
+            print(f"Glückwunsch! Du hast die Zahl {secret_number} in {attempts} Versuchen erraten.")
+            break
+
+if __name__ == "__main__":
+    guess_the_number()
