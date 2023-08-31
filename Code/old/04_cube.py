a105f5220e091319ddb57bb5869482d6e6357347
No0ne155 <magnus@sunrise.ch>
2023-08-31 22:35:20 +0200

    reorganise files

--- Code/old/04_cube.py
+++ Code/old/04_cube.py
@@ -2,7 +2,6 @@ import pygame
 from random import*
 import numpy as np
 
-#Pygame initialisieren
 pygame.init()
 
 #farben
@@ -13,43 +12,26 @@ SCHWARZ = ( 0, 0, 0)
 WEISS   = ( 255, 255, 255)
 GELB    = (255,255,0)
 BLAU    = (0,0,255)
+CLR = (0, 255,255)
 
 #pygame fenster
-screen = pygame.display.set_mode((800, 600))
+screen = pygame.display.set_mode((600, 600))
 pygame.display.set_caption("2x2 Cube Sim")
 screen.fill(SCHWARZ)
+
+class grid:
+    def __init__(self, coord) -> None:
+        self.coord = coord
+    
+    def draw_grid(self, coord):
+        pygame.draw.circle(screen, CLR, ((300+coord[0]),(300+coord[1])), 5)
+
+
+grid0 = grid([0,0])
 
 #pygame loop vorbereiten
 spielaktiv = True
 clock = pygame.time.Clock()
-
-class Cube:
-    def __init__(self, colors, posvec) -> None:
-        self.posvec = posvec
-        self.colors = colors
- 
-    def display(self):
-        if self.posvec == (1,1,1):
-            pygame.draw.polygon(screen, self.colors[0], [[400,300], [487, 250], [400, 200], [313, 250]], 0)
-            pygame.draw.polygon(screen, self.colors[1], [[400,300], [400, 400], [487, 350], [487, 250]], 0)
-            pygame.draw.polygon(screen, self.colors[2], [[400,300], [400, 400], [313, 350], [313, 250]], 0)
-            
-        elif self.posvec == (1,-1,1):
-            pygame.draw.polygon(screen, self.colors[1], [[400,530], [487, 480], [400, 430], [313, 480]], 0)
-            pygame.draw.polygon(screen, self.colors[0], [[400,500], [400, 400], [487, 350], [487, 450]], 0)
-            pygame.draw.polygon(screen, self.colors[2], [[400,500], [400, 400], [313, 350], [313, 450]], 0)
-            
-        elif self.posvec == (-1,-1,1):
-            pygame.draw.polygon(screen, self.colors[1], [[400,530], [487, 480], [400, 430], [313, 480]], 0)
-            pygame.draw.polygon(screen, self.colors[2], [[313,350], [313, 450], [326, 400], [326, 300]], 0)
-            pygame.draw.polygon(screen, self.colors[0], [[400,500], [400, 400], [313, 350], [313, 450]], 0)
-            
-
-
-cube1 = Cube([WEISS, BLAU, ROT], ( 1, 1, 1))
-cube2 = Cube([BLAU, GELB, ROT],  ( 1,-1, 1))
-cube3 = Cube([ROT, GELB, GRUEN], (-1,-1, 1))
-
 
 while spielaktiv:
     # Key events
@@ -68,17 +50,12 @@ while spielaktiv:
                 print("pressed 'arrowdown'")
             elif event.key == pygame.K_SPACE:
                 print("pressed 'SPACE'")
-                cube1.display()
-                cube2.display()
-                cube3.display()
             elif event.key == pygame.K_u:
                 print("pressed 'u'")
             elif event.key == pygame.K_f:
                 print("pressed 'f'")
-                origin_Y = origin_Y -10
             elif event.key == pygame.K_a:
                 print("pressed 'a'")
-                origin_X = origin_X - 10
             elif event.key == pygame.K_s:
                 print("pressed 's'")
             elif event.key == pygame.K_d:
@@ -94,7 +71,7 @@ while spielaktiv:
             print("clicked mouse")
 
 
-
+    grid0.draw_grid([0,0])
     #logik
     
     #Fenster aktualisieren
@@ -104,3 +81,4 @@ while spielaktiv:
     clock.tick(60)
 
 pygame.quit()
+
