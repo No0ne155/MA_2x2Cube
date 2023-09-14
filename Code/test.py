892ae6ba8d8b75dbacfb63df880883f28c3ec5b5
No0ne155 <magnus@sunrise.ch>
2023-09-14 20:51:06 +0200

    new test

--- Code/test.py
+++ Code/test.py
@@ -1,23 +1,12 @@
-import random
+# Die beiden Programme Importieren
+import py222
+import solver
 
-def guess_the_number():
-    secret_number = random.randint(1, 100)
-    attempts = 0
+# Einen Cube genannt myCube erstellen, und in Grundposition definieren
+Cube = py222.initState()
 
-    print("Willkommen bei 'Errate die Zahl'!")
-    print("Ich habe mir eine Zahl zwischen 1 und 100 ausgedacht. Kannst du sie erraten?")
+# Dr Cube führt nun den folgenden Bewegungsablauf durch
+Cube = py222.doAlgStr(Cube, "R U2 R2 F2 R' F2 R F R")
 
-    while True:
-        guess = int(input("Dein Tipp: "))
-        attempts += 1
-
-        if guess < secret_number:
-            print("Zu niedrig! Versuche es erneut.")
-        elif guess > secret_number:
-            print("Zu hoch! Versuche es erneut.")
-        else:
-            print(f"Glückwunsch! Du hast die Zahl {secret_number} in {attempts} Versuchen erraten.")
-            break
-
-if __name__ == "__main__":
-    guess_the_number()
+# Der Cube wird vom solver-Programm gelöst
+solver.solveCube(Cube)
